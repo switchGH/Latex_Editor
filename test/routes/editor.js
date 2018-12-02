4729cd73b910eec14348f2019a5d036e2377ae55
switch <shg18.sk1.hr10@gmail.com>
2018-12-02 21:51:22 +0900

    ajaxを実装

--- test/routes/editor.js
+++ test/routes/editor.js
@@ -5,5 +5,11 @@ router.get('/', (req, res, next) => {
   res.render('editor', {title: 'Latex Editor'});
 });
 
-module.exports = router;
+router.post('/', (req, res, next) => {
+  var obj = {}; 
+  console.log('body: ' + JSON.stringify(req.body.msg));
+  var rejson = JSON.stringify(req.body);
+  res.send(rejson);
+});
 
+module.exports = router;
