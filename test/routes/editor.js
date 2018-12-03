363c58dd7adff1bed0c46cf0c1bdeb31aa39158f
switch <shg18.sk1.hr10@gmail.com>
2018-12-03 11:00:45 +0900

    jsonファイルを書き出す

--- test/routes/editor.js
+++ test/routes/editor.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res, next) => {
   res.render('editor', {title: 'Latex Editor'});
@@ -8,6 +9,7 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   var obj = {}; 
   console.log('body: ' + JSON.stringify(req.body.msg));
+  fs.writeFileSync('sample.json', JSON.stringify(req.body.msg));
   var rejson = JSON.stringify(req.body);
   res.send(rejson);
 });
