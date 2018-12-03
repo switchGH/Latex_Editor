fae6aae6ffa5653c5dcef9b20234364d28dfda34
switch <shg18.sk1.hr10@gmail.com>
2018-12-03 20:56:38 +0900

    構文ボタンを追加

--- test/routes/editor.js
+++ test/routes/editor.js
@@ -8,8 +8,15 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   var obj = {}; 
-  console.log('body: ' + JSON.stringify(req.body.msg));
-  fs.writeFileSync('sample.json', JSON.stringify(req.body.msg));
+  //console.log('body: ' + JSON.stringify(req.body.msg));
+  jsonData = JSON.stringify(req.body.msg);
+  var arr = jsonData.split(/\r\n|\\n/);
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  }
+  fs.writeFileSync('sample.tex', arr);
+  //fs.writeFileSync('sample.txt', JSON.stringify(req.body.msg));
   var rejson = JSON.stringify(req.body);
   res.send(rejson);
 });
