521d6f80c06857b219d127f339f0abf77e8343e6
switch <shg18.sk1.hr10@gmail.com>
2018-12-05 12:18:21 +0900

    ダブルスラッシュ不具合を修正

--- test/routes/editor.js
+++ test/routes/editor.js
@@ -7,15 +7,23 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  var obj = {}; 
   jsonData = JSON.stringify(req.body.msg);
+  console.log(jsonData);
   var arr = jsonData.split(/\\n/);
   for (var i = 0; i < arr.length; i++){
     console.log(arr[i]);
+    arr[i] = arr[i].replace(/\\\\/g, '\\');
     fs.appendFileSync('sample.tex', arr[i] + '\n', function (err) {
       console.log(err);
     });
   }
+  // jsonData = JSON.parse(req.body.msg);
+  // // jsonDataにユーザーが入力した文字列が入っている
+  // console.log(jsonData);
+
+  // fs.writeFileSync('sample.tex', jsonData, function (err) {
+  //   console.log(err);
+  // });
   var rejson = JSON.stringify(req.body);
   res.send(rejson);
 });
