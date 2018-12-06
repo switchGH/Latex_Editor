ef14f86c1d9caf6695948ddddcc4575dc1a150c1
switch <shg18.sk1.hr10@gmail.com>
2018-12-06 09:41:22 +0900

    \newpageの処理を保留

--- test/routes/editor.js
+++ test/routes/editor.js
@@ -9,14 +9,19 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   jsonData = JSON.stringify(req.body.msg).slice(1, -1);
   //jsonを受け取り、文頭・文末の「"」を削除する
-  var arr = jsonData.split(/\\n/);//改行で分割し、配列に格納する
+  var arr = jsonData.split(/\\n/);
+  //var arr = jsonData.split(/(?!\\\\newpage)(?=\\n)/);
+  // \newpageの処理は保留
   for (var i = 0; i < arr.length; i++){
-    console.log(arr[i]);
+    //console.log(arr[i]);
     arr[i] = arr[i].replace(/\\\\/g, '\\');
     fs.appendFileSync('sample.tex', arr[i] + '\n', function (err) {
       console.log(err);
     });
   }
+  // fs.writeFileSync('sample.tex', jsonData, function (err) {
+  //   console.log(err);
+  // });
   res.send(jsonData);
 });
 
